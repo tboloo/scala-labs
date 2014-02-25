574c140060a3b4df81064c4ddce325acd7e706d2
Bolesław Tekielski <btekielski@gmail.com>
2014-02-25 16:24:01 +0100

    ListManipulationExercise02

--- labs/src/main/scala/org/scalalabs/basic/lab02/ListManipulationExercise02.scala
+++ labs/src/main/scala/org/scalalabs/basic/lab02/ListManipulationExercise02.scala
@@ -1,7 +1,7 @@
 package org.scalalabs.basic.lab02
 
 import scala.collection.mutable.ListBuffer
- import sys._
+import sys._
 
 
 object ListManipulationExercise02 {
@@ -10,24 +10,26 @@ object ListManipulationExercise02 {
    * Find the maximum element in a list, e.g. maxElementInList(List(1,9,3,5)) == 9
    * As usual, various ways exist: pattern matching, folding, ...
    */
-  def maxElementInList(l: List[Int]): Int = {
-    error("fix me")
+   def maxElementInList(l: List[Int]): Int = {
+    l.foldLeft(l.head)((acc,e) => if (e > acc) e else acc)
   }
 
   /**
    * Calculate the sum of the equally position elements
    * of the two list
    */
-  def sumOfTwo(l1: List[Int], l2: List[Int]): List[Int] = {
-    error("fix me")
+   def sumOfTwo(l1: List[Int], l2: List[Int]): List[Int] = (l1, l2) match {
+    case (_, Nil) => l1
+    case (Nil, _) => l2
+    case _ => (l1 zip l2) map (tuple => tuple._1 + tuple._2)
   }
 
   /**
    *  For this exercise preferably make use of the sumOfTwo
    * method above
    */
-  def sumOfMany(l: List[Int]*): List[Int] = {
-    error("fix me")
+   def sumOfMany(l: List[Int]*): List[Int] = {
+    l.foldLeft(List[Int]())((acc,e) => sumOfTwo(acc,e))
   }
 
   case class Person(age: Int, firstName: String, lastName: String)
@@ -38,30 +40,33 @@ object ListManipulationExercise02 {
    * may be able to achieve the same functionality as implemented below
    * in a one-liner.
    */
-  def separateTheMenFromTheBoys(persons: List[Person]): List[List[String]] = {
+   def separateTheMenFromTheBoysImperative(persons: List[Person]): List[List[String]] = {
     var boys: ListBuffer[Person] = new ListBuffer[Person]()
     var men: ListBuffer[Person] = new ListBuffer[Person]()
     var validBoyNames: ListBuffer[String] = new ListBuffer[String]()
     var validMenNames: ListBuffer[String] = new ListBuffer[String]()
 
     for (person <- persons) {
-        if (person.age < 18) {
-          boys += person
+      if (person.age < 18) {
+        boys += person
         } else {
           men += person
         }
+      }
+
+      var sortedBoys = boys.toList.sortBy(_.age)
+      var sortedMen = men.toList.sortBy(_.age)
+
+      for (boy <- sortedBoys) {
+        validBoyNames += boy.firstName
+      }
+      for (man <- sortedMen) {
+        validMenNames += man.firstName
+      }
+      List(validBoyNames.toList, validMenNames.toList)
     }
 
-    var sortedBoys = boys.toList.sortBy(_.age)
-    var sortedMen = men.toList.sortBy(_.age)
-
-    for (boy <- sortedBoys) {
-      validBoyNames += boy.firstName
-    }
-    for (man <- sortedMen) {
-      validMenNames += man.firstName
-    }
-    List(validBoyNames.toList, validMenNames.toList)
+    def separateTheMenFromTheBoys(persons: List[Person]): List[List[String]] = 
+    List(persons.filter(_.age<18).sortBy(_.age) map (_.firstName), 
+      persons.filter(_.age>=18).sortBy(_.age) map (_.firstName))
   }
-
-}
